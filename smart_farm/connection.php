515d6277b27061cb690bc30913669ba50bd00141
Dzaki Prasetyo Iswandanu <182347485+Agera-One@users.noreply.github.com>
2025-09-23 19:45:52 +0700

    Delete smart_farm/connection.php

--- smart_farm/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "127.0.0.1:3307";
-$username = "root";
-$password = "";
-$database = "smart_farm";
-
-$connection = mysqli_connect($hostname, $username, $password, $database);
-mysqli_select_db($connection, $database);
